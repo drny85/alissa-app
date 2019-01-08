9b12bd8053cc1e356cf8b0a89ebc3b8dfe84adf6
Robert Melendez <robertm3lendez@gmail.com>
2019-01-08 02:19:58 -0500

    ch

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   stripeKey: "pk_test_ETHpUWvdYq4OZdsKZoInMVGy",
-  apiURL: "http://alissafitness-env.bvmcf7fq2w.us-east-2.elasticbeanstalk.com/"
+  apiURL: ""
   // apiURL: "http://localhost:3000/"
 };
